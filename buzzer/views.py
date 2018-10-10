da1265e70fc63227fbb974bb3cd7d82aa8c18bb7
NibrassHaider <nhaideha7@alumnes.ub.edu>
2018-10-10 17:56:54 +0200

    Data Persistence

--- buzzer/views.py
+++ buzzer/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Buser
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def index(request):
@@ -20,3 +21,51 @@ def users(request, user=""):
         response = response + '<BR> <li>' + '<BR> <li>'.join([Buser.all_fields(user.buser) for user in list_of_users])
 
     return HttpResponse(response)
+
+def signupView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # Log in the user after the registration
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = UserCreationForm()
+    return render(request, "signup.html", {'form': form})
+
+
+def loginView(request):
+    username = request.POST.get('username', '')
+    print(username)
+    password = request.POST.get('password', '')
+    print(password)
+    user = authenticate(username=username, password=password)
+    print(user)
+
+
+    if user is not None:
+        if user.is_active:  # Active user are not banned users
+            login(request, user)
+
+            # Redirect to a success page.
+            #return HttpResponseRedirect(reverse('index'))
+            return render(request, 'Home_Page.html')
+
+        else:   # User is banned
+            raise forms.ValidationError(_("This account is banned."), code='inactive',)
+    else:
+        # Show an error page
+        return render(request, 'login.html')
+
+
+def logoutView(request):
+    logout(request)
+    # Redirect to a success page.
+    return render(request, 'login.html')
+
