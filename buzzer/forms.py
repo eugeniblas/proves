92810dde86ee1a6ad71ba3db37d9abd464fa28fe
NibrassHaider <nhaideha7@alumnes.ub.edu>
2018-10-20 21:32:04 +0200

    Testing

--- buzzer/forms.py
+++ buzzer/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Post
+from .models import Buzz
 
 
 class PostForm(forms.ModelForm):
     class Meta :
-        model = Post
+        model = Buzz
         fields = ('title','text',)
         
--- buzzer/models.py
+++ buzzer/models.py
@@ -54,7 +54,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
+"""class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -69,4 +69,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        """
 
